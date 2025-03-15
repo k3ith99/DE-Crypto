13188338d3817e45585039f5ac2031467023d99d
hamza <hamza_saleh1999@hotmail.com>
2025-03-15 14:38:19 +0000

    init minio monthly pipeline and start building the image

--- src/minio/bucket-init.sh
+++ src/minio/bucket-init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #sleep 5
+
 mc alias set local $ADDRESS $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 mc alias list
 mc admin info local 
-mc mb local/binance_data
+mc mb local/binancedata
 mc admin accesskey create local/
 #for i in BTC ETH LTC DOGE BNB
 #do
